8b945c1457d9f4ff4d5693d0a7b4c94d7dcab465
Tish254 <tiegomseeraoscar295@gmail.com>
2022-08-29 00:35:28 +0300

    Programs section Home page done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainHeader from '../../components/MainHeader'
+import Programs from '../../components/Programs'
 import './home.css'
 
 const Home = () => {
   return (
     <>
       <MainHeader />
+      <Programs />
     </>
   )
 }
